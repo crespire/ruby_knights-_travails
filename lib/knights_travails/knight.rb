cec0dd6c566a551ae1565c7a9d9b2058b557374e
crespire <li.simmon@gmail.com>
2021-11-26 16:07:04 -0500

    Add private coords conversion methods

--- lib/knights_travails/knight.rb
+++ lib/knights_travails/knight.rb
@@ -5,12 +5,12 @@ module KnightsTravails
     end
 
     def knight_moves(orig, term)
-      # From cell, calculate
+      valid = valid_moves(orig)
     end
 
     private
 
-    def translate_coords(input)
+    def find_al_coords(input)
       return nil unless input&.length == 2
       return nil unless input&.reduce(:+).between?(2, 16)
 
@@ -20,6 +20,18 @@ module KnightsTravails
       alpha_i, num_i = input
 
       "#{letter[alpha_i - 1]}#{number[num_i - 1]}"
+    end
+
+    def find_array_coords(input)
+
+    end
+
+    def valid_moves(node = @current_cell)
+      # This method should generate an edge list from a given node.
+      # We can just keep calling this, so this method should just return the
+      # set of edges that are next.
+
+
     end
   end
 end
