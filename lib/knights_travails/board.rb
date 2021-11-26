0d04eb5583838f52be85f5442ce66271a89f29ac
crespire <li.simmon@gmail.com>
2021-11-26 15:50:30 -0500

    Linter pass

--- lib/knights_travails/board.rb
+++ lib/knights_travails/board.rb
@@ -10,11 +10,11 @@ module KnightsTravails
       letter = %w[a b c d e f g h]
       number = %w[1 2 3 4 5 6 7 8]
 
-      letter.each { |l|
-        number.each { |n|
+      letter.each do |l|
+        number.each do |n|
           @board["#{l}#{n}".to_sym] = Cell.new("#{l}#{n}")
-        }
-      }
+        end
+      end
     end
   end
 end
--- lib/knights_travails/knight.rb
+++ lib/knights_travails/knight.rb
@@ -4,12 +4,15 @@ module KnightsTravails
       @current_cell = cell
     end
 
-    def knight_moves(start, end)
+    def knight_moves(orig, term)
       # From cell, calculate
     end
 
+    private
+
     def translate_coords(input)
-      return nil unless input.is_a? Array && input.length == 2
+      return nil unless input&.length == 2
+      return nil unless input&.reduce(:+).between?(2, 16)
 
       letter = %w[a b c d e f g h]
       number = %w[1 2 3 4 5 6 7 8]
